3da9d8f8ebd0f7a545bc1e3fc948eb83c553f62a
Jefferson <jeffersonpierov@gmail.com>
2022-10-10 15:11:34 -0500

    version1.8

--- js/main.js
+++ js/main.js
@@ -4,77 +4,100 @@ const btn_menu = document.querySelector('#btn-menu')
 const div_mobile = document.querySelector('#nav-mobile');
 
 
-window.sr= ScrollReveal();
-sr.reveal('.container-about',{
-  duration:4000,
-  origin:'left',
-  distance:'50px',
+window.sr = ScrollReveal();
+sr.reveal('.container-about', {
+  duration: 4000,
+  origin: 'left',
+  distance: '50px',
 });
-sr.reveal('.section-title',{
-  duration:4000,
-  origin:'right',
-  distance:'50px',
+sr.reveal('.section-title', {
+  duration: 4000,
+  origin: 'right',
+  distance: '50px',
 });
-sr.reveal('.section-title',{
-  duration:4000,
-  origin:'right',
-  distance:'50px',
+sr.reveal('.section-title', {
+  duration: 4000,
+  origin: 'right',
+  distance: '50px',
 });
-sr.reveal('.container-card-estudios',{
-  duration:4000,
-  origin:'right',
-  distance:'50px',
+sr.reveal('.container-card-estudios', {
+  duration: 4000,
+  origin: 'right',
+  distance: '50px',
 });
-sr.reveal('.container-card-portfolio',{
-  duration:4000,
-  origin:'right',
-  distance:'50px',
+sr.reveal('.container-card-portfolio', {
+  duration: 4000,
+  origin: 'right',
+  distance: '50px',
 });
 
 
-btn_menu.addEventListener('click',function(){
-    div_mobile.classList.toggle('active')
+btn_menu.addEventListener('click', function () {
+  div_mobile.classList.toggle('active');
+ 
 })
 
-const typed = new Typed('.typed',{
-  strings:['Developer','Freelance'],
-  typeSpeed:40,
-  backSpeed:20,
-  loop:true,
+const typed = new Typed('.typed', {
+  strings: ['Developer', 'Freelance'],
+  typeSpeed: 40,
+  backSpeed: 20,
+  loop: true,
 });
 
 let btn_correo = document.getElementById('btn-enviar');
-const form =document.getElementById('form');
- 
-btn_correo.addEventListener('click',(e)=>{
+const form = document.getElementById('form');
+
+btn_correo.addEventListener('click', (e) => {
   e.preventDefault();
-  btn_correo.value = "Enviando...";
-  const serviceID = 'default_service';
-  const templateID = 'template_e5vh609';
-  
-  emailjs.sendForm(serviceID,templateID,form)
-  .then(()=>{
-    btn_correo.value = 'Enviar Correo Electronico';
-    alert('Enviado!');
-  },(err)=>{
-    btn_correo.value = 'Email no enviado';
-    alert(JSON.stringify(err));
-  });
+  const txt_name = document.getElementById('from_name');
+  const txt_email = document.getElementById('email_id');
+  const txt_message = document.getElementById('message');
+
+  if (
+    txt_name.value == '' ||
+    txt_email.value == '' ||
+    txt_message.value == '') {
+    Swal.fire({
+      title: 'Ingresar Datos!',
+      text: 'Colocar sus datos requeridos',
+      icon: 'error',
+      confirmButtonText: 'Regresar'
+    })
+  } else {
+    btn_correo.value = "Enviando...";
+    const serviceID = 'default_service';
+    const templateID = 'template_e5vh609';
+
+    emailjs.sendForm(serviceID, templateID, form)
+      .then(() => {
+        btn_correo.value = 'Enviar Correo Electronico';
+        Swal.fire({
+          position:'top-end',
+          icon:'success',
+          title:'Datos enviado Correctamente',
+          showConfirmButton:false,
+          timer:1200
+        });
+      }, (err) => {
+        btn_correo.value = 'Email no enviado';
+        alert(JSON.stringify(err));
+      });
+  }
   form.reset();
 });
 
 
 const btn_cv = document.getElementById('btn-cv');
 
-const dowload = (filename)=>{
+const dowload = (filename) => {
   let element = document.createElement('a');
-  element.setAttribute('href','Cv.pdf');
-  element.setAttribute('dowload',filename);
+  element.setAttribute('href', 'Cv.pdf');
+  element.setAttribute('dowload', filename);
   document.body.appendChild(element);
   element.click();
 }
 
-btn_cv.addEventListener('click',function(){
+btn_cv.addEventListener('click', function () {
   let filename = "cv.pdf";
   dowload(filename);
-},false);
+}, false);
